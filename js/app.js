69e49a4421ccb36bedbb6a40aad4d0e937a82e6e
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-22 22:59:41 -0300

    Small JS refactor

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-  $('#carousel').slick({
-    'dots': true,
-    'nextArrow': '<button type="button" class="slick-next">&gt;</button>',
-    'prevArrow': '<button type="button" class="slick-prev">&lt;</button>'
-  });
+  $('#carousel').slick(carouselSettings);
 });
+
+var carouselSettings = {
+  'dots': true,
+  'nextArrow': '<button type="button" class="slick-next">&gt;</button>',
+  'prevArrow': '<button type="button" class="slick-prev">&lt;</button>'
+};
