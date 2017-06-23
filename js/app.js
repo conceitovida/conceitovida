d0aaea02a23ee1c5b93679dc2d63c05305bbd92e
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-22 23:00:11 -0300

    Add home main text

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
+
+  // carousel
   $('#carousel').slick(carouselSettings);
+
+  // home
+  fixHomeMain();
+  $(window).resize(fixHomeMain);
+
 });
 
 var carouselSettings = {
   'dots': true,
   'nextArrow': '<button type="button" class="slick-next">&gt;</button>',
   'prevArrow': '<button type="button" class="slick-prev">&lt;</button>'
+};
+
+var fixHomeMain = function () {
+  var homeMain = $('main.home');
+
+  if (homeMain.height() !== undefined) {
+    var height = 'auto';
+    homeMain.find('div').css('height', 'auto');
+
+    if($(window).width() >= 768) {
+      height = homeMain.height() + 'px';
+      homeMain.find('div').css('height', height);
+    }
+
+  }
 };
