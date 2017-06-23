47dc69891f83abc4b96bccac07162577eef02935
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-22 22:05:45 -0300

    Add carousel to home

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#carousel').slick({
+    'dots': true,
+    'nextArrow': '<button type="button" class="slick-next">&gt;</button>',
+    'prevArrow': '<button type="button" class="slick-prev">&lt;</button>'
+  });
+});
